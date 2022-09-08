31b679e9c2220203482ade1a3fcb55b423d927cc
ahphoo <aphu@localhost.localdomain>
2022-09-08 01:17:31 -0700

    Added Linked List dir. Added solution for 'Construct Binary Tree from Preorder and Inorder Traversal'.

---
+++ Linked List/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # O(n) time | O(n) space
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        i = self.findIdx(inorder, root.val)
+
+        root.left = self.buildTree(preorder[1:i+1], inorder[:i])
+        root.right = self.buildTree(preorder[i+1:], inorder[i+1:])
+
+        return root
+
+    def findIdx(self, inorder, val):
+        for i in range(len(inorder)):
+            if inorder[i] == val:
+                return i
+        return -1  # should never reach here
